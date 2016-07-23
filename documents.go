e1c2fdaf84211bd57f3dee6314cc4c1ec1bc4131
jndewey <jndewey@gmail.com>
2016-07-23 16:56:46 -0400

    msc2

---
+++ documents.go
@@ -0,0 +1,55 @@
+package cctools
+
+import "log"
+import "encoding/json"
+
+
+//Conversion Functions
+//JSON
+func ClauseToJSON(c Clause) []byte {
+	data, err := json.MarshalIndent(c, "", "	")
+	if err != nil {
+		log.Fatalf("JSON conversion failed: %s", err)
+	}
+	return data
+} 
+
+func JSONtoClause(data []byte) Clause {
+	var c Clause
+	json.Unmarshal(data, &c)
+	return c
+} 
+
+//YAML
+//TO BE ADDED
+
+//Creation of Clause struct, including corresponding functions
+
+type Clause struct {
+	Name	string		`json:"name"`
+	Text	string		`json:"text"`
+}
+
+func ReturnName(c Clause) string {
+	var output = c.Name
+	return output
+}
+
+func ReturnText(c Clause) string {
+	var output = c.Text
+	return output
+}
+
+//Creation of Document struct, including corresponding functions
+
+type Document struct {
+	Id 			string
+	FormNumber	int
+	FornName	string
+	Clauses		[]Clause
+}
+
+func NumberOfClauses(d Document) int {
+	return len(d.Clauses)
+}
+
