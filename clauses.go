e61ab80bde43a4c98e33645840c5e7470a472be8
jndewey <jndewey@gmail.com>
2016-08-10 17:32:35 -0400

    more tools

--- clauses.go
+++ clauses.go
@@ -11,27 +11,11 @@ type Clause struct {
 	BftPty		Party		`json:"bftpty"` //If one or more parties benefit from a condition (to the exclusion of others), then BftPty identifies those parties
 }
 
-/* Getter for Name attribute
-
-func (c Clause) Name() string {
-	var output = c.name
-	return output
-}
-*/
-
 //Setter for Name attribute
 
 func (c *Clause) SetName(n string) {
 	c.Name = n
 }
-
-/* Getter for Text attribute
-
-func (c Clause) Text() string {
-	var output = c.Text
-	return output
-}
-*/
 
 //Setter for Text attribute
 
@@ -39,43 +23,17 @@ func (c *Clause) SetText(t string) {
 	c.Text = t
 }
 
-/* Getter for Type attribute
-//Initially supported types will be Definition, Covenant, Condition, 
-//Representation (includes warranties) and Information (e.g., notice)
-
-func (c Clause) Type() string {
-	var output = c.Type
-	return output
-}
-*/
-
 //Setter for Type attribute
 
 func (c *Clause) SetType(t string) {
 	c.Type = t
 }
 
-/* Getter for Benefit attribute
-
-func (c Clause) Benefit() bool {
-	var output = c.Benefit
-	return output
-}
-*/
-
 //Setter for Benefit attribute
 
 func (c *Clause) SetBenefit(b bool) {
 	c.Benefit = b
 }
-
-/* Getter for BftPty attribute
-
-func (c Clause) BftPty() Party {
-	var output = c.BftPty
-	return output
-}
-*/
 
 //Setter for Benefit attribute
 //Generally, the benefitted party has the right to waive a condition and proceed with the transaction
