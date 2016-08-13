df4bf2d3e3be0efd0904f872173fa294c46c60db
jndewey <jndewey@gmail.com>
2016-08-12 21:32:50 -0400

    parser works

--- utilities.go
+++ utilities.go
@@ -1,11 +1,9 @@
 package cctools
 
-//import "io/ioutil"
 import "html/template"
-import "os"
 import "fmt"
 import "bytes"
-import "io"
+
 
 // Text Generators -- These tools convert Clauses and Documents into final text documents by combining parameter values with
 // text templates for each clause or an entire Document.
@@ -13,28 +11,16 @@ import "io"
 //This function accepts a pointer to a Clause and renders the template text by passing the Parameter values to the template text
 //and afterwards saves the resulting merged text to the MergedText attribute of the Clause
 
-func CaptureStdout(f func()) string {
-  old := os.Stdout
-  r, w, _ := os.Pipe()
-  os.Stdout = w
-
-  f()
-
-  w.Close()
-  os.Stdout = old
-
-  var buf bytes.Buffer
-  io.Copy(&buf, r)
-  return buf.String()
-}
-
 func (c *Clause) ParseClause() {
 	t, err := template.New(c.Name).Parse(c.Text)
 	if err != nil {
 		fmt.Println(err)
 	}
 	varmap := c.Params
-	t.ExecuteTemplate(os.Stdout, "TextForTemplate", varmap)
+	data := new(bytes.Buffer)
+	t.ExecuteTemplate(data, "TextForTemplate", varmap)
+	output := data.String()
+	c.MergedText = output
 }
 
 // Compliance Utilities - Pass Documents and Clauses to method that takes a particular compliance module
