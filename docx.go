cffa7e05e4891fbc87f962f544ea02324f0201e3
jndewey <jndewey@gmail.com>
2016-08-14 13:33:04 -0400

    readme

--- docx.go
+++ docx.go
@@ -4,13 +4,13 @@ import (
 	"os"
 	"fmt"
 	"encoding/xml"
-	"io/ioutil"
+	"io"
 )
 
 // this will include utilities to assist with working with .docx files
 
-type WordDocumentXML struct {
-	Plane	string	`xml:"Plane"`
+type Plane struct {
+	Plane	string	`xml:"plane"`
 }
 
 func ParseXML(filename string) {
@@ -20,19 +20,34 @@ func ParseXML(filename string) {
 		return
 	}
 	defer xmlFile.Close()
+	/*
 	b, err := ioutil.ReadAll(xmlFile)
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
-	var doc WordDocumentXML
-	doc.Plane = "default"
-	xml.Unmarshal(b, &doc)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
+	*/
+	decoder := xml.NewDecoder(xmlFile)
+	for {
+		t, err := decoder.Token()
+		ct := xml.CopyToken(t)
+		fmt.Println(ct)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "plane" {
+				var plane Plane
+				decoder.DecodeElement(&plane, &se)
+				fmt.Println(plane.Plane)
+			}
+		}
 	}
-	fmt.Println(doc.Plane)
 }
 
 
